ba762934f09f5fd5e3af74c8f6eac7abe9c2b69b
Ezra Schwepker <ezraschwepker@gmail.com>
2018-09-18 14:24:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+ return document.querySelector('#nested div div div .target');
+}
+
+function increaseRankBy(n) {
+  for (let each in document.querySelectorAll('.ranked-list')) {
+    console.log("test");
+    each.innerHTML = parseInt(each.innerHTML) + 1;
+  }
+}
+
+function deepestChild() {
+  let
+}
